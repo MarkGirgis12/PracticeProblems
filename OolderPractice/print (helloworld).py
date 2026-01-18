a260d052de488c07308573fb2d757f54b029c4f5
MarkGirgis12 <girgismark2005@gmail.com>
2026-01-18 17:22:34 -0500

    print hellow world practice

--- OolderPractice/print (helloworld).py
+++ OolderPractice/print (helloworld).py
@@ -4,3 +4,8 @@ def add (x, y):
 print(add(2, 3))
 print("Hello, World!")
 
+print("This is a sample Python script.")
+print("Let's perform some addition:")   
+result = add(10, 5)
+print("The result of adding 10 and 5 is:", result)
+print("Goodbye, World!")    
